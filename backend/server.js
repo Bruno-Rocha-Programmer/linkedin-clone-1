d702de413a71d06ce726d0f948166604745a9b01
Bruno-Rocha-Programmer <robertbruno592@gmail.com>
2025-07-01 17:15:57 -0300

    test

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,6 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV !== "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
   app.use(
     cors({
       origin: "http://localhost:5173",
